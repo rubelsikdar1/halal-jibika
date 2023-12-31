177c5084aa63cff8160a1c2fffc415fe0b7a0e66
rubelsikdar1 <rubelsidar799@hotmail.com>
2023-12-31 11:21:56 +0600

    [Done] all jobs boxt create and component

---
+++ src/Components/JobsComponents/SingleJobList/SingleJobList.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import Style from "./SingleJobList.module.css";
+
+useEffect(()=>{
+    
+},[])
+
+const SingleJobList = () => {
+    return (
+        <div>
+            <div className={Style.mainBox}>
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default SingleJobList;
--- src/Pages/Jobs/Jobs.jsx
+++ src/Pages/Jobs/Jobs.jsx
@@ -1,9 +1,20 @@
-
+import SingleJobList from "../../Components/JobsComponents/SingleJobList/SingleJobList";
+import Style from "./Jobs.module.css";
 
 const Jobs = () => {
     return (
-        <div className="container">
-            <h1>this is job page</h1>
+        <div className={Style.jobBody}>
+            <h1 className={Style.header}>All JOBS</h1>
+            <div className= "container">
+                <div className={Style.MainContainer}>
+                    <div className={Style.showLIst}>
+                        <SingleJobList/>
+                    </div>
+                    <div className={Style.RightSidebar}>
+                        <SingleJobList/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
